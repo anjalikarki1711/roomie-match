d97a898b4ef1db4ad01209faf3f7528ca351190e
Anjali Karki <anjalikarki1711>
2024-11-18 17:47:30 -0500

    Added th template along with routes and all the html files, and style. Changes in all html filesalong with app.py, and style.css

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+# one or the other of these. Defaults to MySQL (PyMySQL)
+# change comment characters to switch to SQLite
+
+import cs304dbi as dbi
+# import cs304dbi_sqlite3 as dbi
+
+import secrets
+
+app.secret_key = 'your secret here'
+# replace that with a random key
+app.secret_key = secrets.token_hex()
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+@app.route('/')
+def index():
+    return render_template('home.html',
+                           page_title='Home Page')
+
+@app.route('/makePost/', methods=["GET", "POST"])
+def makePosts():
+    return render_template('makePosts.html',
+                           page_title='Make a Post')
+@app.route('/feed/', methods=["GET", "POST"])
+def viewPosts():
+    return render_template('feed.html',
+                           page_title='Posts')
+
+@app.route('/profile/', methods=["GET", "POST"])
+def viewProfile():
+    return render_template('viewProfile.html',
+                           page_title='Profile')
+
+@app.route('/editProfile/<uid>', methods=["GET", "POST"])
+def editProfile():
+    return render_template('editProfile.html',
+                           page_title='Edit Profile')
+
+@app.route('/chat/', methods=["GET", "POST"])
+def viewChat():
+    return render_template('chat.html',
+                           page_title='Chat History')
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    # set this local variable to 'wmdb' or your personal or team db
+    db_to_use = 'roomie_match_db' 
+    print(f'will connect to {db_to_use}')
+    dbi.conf(db_to_use)
+    app.debug = True
+    app.run('0.0.0.0',port)
