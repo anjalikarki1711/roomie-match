6e6a7a2e1ebf644ec0d98d67c0402d38b39ef886
Indira Fall 24 <rimindirarus@gmail.com>
2024-11-24 15:50:29 -0500

    Updated app.py with post functionality and moved login to a separate file like how it was before

--- app.py
+++ app.py
@@ -7,124 +7,22 @@ app = Flask(__name__)
 # change comment characters to switch to SQLite
 
 import cs304dbi as dbi
-# import cs304dbi_sqlite3 as dbi
-
 import sys, os, random
 import bcrypt
 import secrets
+from werkzeug.utils import secure_filename
+import homepage as homepage
+import login as login
+from flask import g
+
 
 app.secret_key = ''.join([random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for _ in range(20)])
 app.config['UPLOADS'] = os.path.expanduser('~/cs304/roomie-match/uploads/profile_pics')  # Directory for profile picture uploads
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  # Max file size: 1 MB
+app.config['MAX_CONTENT_LENGTH'] = 5*1024*1024 # 5 MB
 
 # This gets us better error messages for certain common request errors
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 
-from flask import g
-
-# Ali's sign up, log in, and log out
-@app.route('/sign-up/', methods=["GET", "POST"])
-def join():
-    if request.method == "GET":
-        return render_template ("sign-up.html", page_title="Sign Up")
-
-    
-    else: 
-        username = request.form.get('user-name')
-        passwd1 = request.form.get('password1')
-        passwd2 = request.form.get('password2')
-        if passwd1 != passwd2:
-            flash('passwords do not match')
-            return redirect( url_for('join'))
-        
-        hashed = bcrypt.hashpw(passwd1.encode('utf-8'),
-                            bcrypt.gensalt())
-        stored = hashed.decode('utf-8')
-        print(passwd1, type(passwd1), hashed, stored)
-
-        #connect to database
-        conn = dbi.connect()
-        curs = dbi.cursor(conn)
-        try:
-            curs.execute('''INSERT INTO login(user_name,hashed_password)
-                            VALUES(%s,%s)
-                         RETURNING user_id''',
-                        [username, stored])
-            user_id = curs.fetchone()[0]
-
-            # Insert the user_id into the user table
-            curs.execute('''INSERT INTO user(user_id)
-                VALUES(%s)''',
-             [user_id])
-            
-            conn.commit()
-        except Exception as err:
-            flash('That email is tied to an existing account: {}'.format(repr(err)))
-            return redirect(url_for('index'))
-        curs.execute('select last_insert_id()')
-        row = curs.fetchone()
-        uid = row[0]
-        flash('FYI, you were issued UID {}'.format(uid))
-        session['username'] = username
-        session['uid'] = uid
-        session['logged_in'] = True
-        session['visits'] = 1
-        return redirect( url_for('viewProfile' ) ) #, username=username) )
-
-@app.route('/login/', methods=["GET", "POST"])
-def login():
-    if request.method == "GET":
-        return render_template ("login.html", page_title="Log In")
-    
-    else: 
-        username = request.form.get('username')
-        passwd = request.form.get('password')
-        conn = dbi.connect()
-        curs = dbi.dict_cursor(conn)
-        curs.execute('''SELECT user_id,user_name,hashed_password
-                        FROM login
-                        WHERE user_name = %s''',
-                    [username])
-        row = curs.fetchone()
-        if row is None:
-            # Same response as wrong password,
-            # so no information about what went wrong
-            flash('login incorrect. Try again or join')
-            return redirect( url_for('login'))
-        
-        stored = row['hashed_password']
-        print('database has stored: {} {}'.format(stored,type(stored)))
-        print('form supplied passwd: {} {}'.format(passwd,type(passwd)))
-        hashed2 = bcrypt.hashpw(passwd.encode('utf-8'),
-                                stored.encode('utf-8'))
-        hashed2_str = hashed2.decode('utf-8')
-        print('rehash is: {} {}'.format(hashed2_str,type(hashed2_str)))
-        if hashed2_str == stored:
-            print('Session before redirect: ', session)  # Log session before redirect
-            print('they match!')
-            flash('successfully logged in as '+username)
-            session['username'] = username
-            session['user_id'] = row['user_id']
-            session['logged_in'] = True
-            session['visits'] = 1
-            print('Session after login: ', session)  # Session after login
-            return redirect( url_for('viewProfile' ) ) #, username=username) )
-        else:
-            flash('password incorrect. Try again or join')
-            return redirect( url_for('join'))
-        
-@app.route('/logout/')
-def logout():
-    if 'username' in session:
-        username = session['username']
-        session.pop('username')
-        session.pop('user_id')
-        session.pop('logged_in')
-        flash('You are logged out')
-        return redirect(url_for('index'))
-    else:
-        flash('you are not logged in. Please login or join')
-        return redirect( url_for('index') )
 
 """ @app.route('/test-login')
 def test_login():
@@ -147,12 +45,78 @@ def index():
 
 @app.route('/makePost/', methods=["GET", "POST"])
 def makePosts():
-    return render_template('makePosts.html',
-                           page_title='Make a Post')
+    if 'user_id' in session:
+        uid = session['user_id']
+        if request.method == 'GET':
+            return render_template('makePosts.html',
+                            page_title='Make a Post')
+        else: 
+            conn = dbi.connect()
+            curs = dbi.dict_cursor(conn)
+            #use last_inserted_id to get the post id
+            curs.execute('''
+                        select last_insert_id''')
+            pidDict = curs.fetchone()
+            pid = pidDict['last_insert_id()']
+            #retrieves form data 
+            p_type = request.form.get('post_type')
+            h_type = request.form.get('housing_type')
+            rent = request.form.get('budget')
+            roommatesNum = request.form.get('max_roommates')
+            sbed = request.form.get('shared_bedroom')
+            sbath = request.form.get('mshared_bathroom')
+            pets = request.form.get('ok_with_pets')
+            description = request.form.get("descr")
+            f = request.files['pic']
+            user_filename = f.filename
+            ext = user_filename.split('.')[-1]
+            filename = secure_filename('{}_{}.{}'.format(pid, uid, ext))
+            pathname = os.path.join(app.config['UPLOADS'],filename)
+            f.save(pathname)
+
+        #insert into the database
+        #checks wether the inputs are integers
+            if homepage.isInt(rent) and homepage.isInt(roommatesNum):
+                curs.execute(
+                    '''insert into post(user_id, shared_bathroom, shared_bedroom, 
+                    ok_with_pets, max_roommates, budget, housing_type, post_type) 
+                    values (%s,%s,%s,%s,%s,%s,%s,%s)''',
+                    [uid, sbath, sbed, pets, roommatesNum, rent, h_type, p_type])
+
+                curs.execute(
+                    '''insert into file(user_id, post_id, room_pic) values (%s,%s, %s)
+                        ''', [uid, pid, filename])
+                conn.commit()
+                flash('Post successful')
+                return redirect(url_for('viewPosts'))
+            else:
+                flash('Budget and max_number of roomates should be integers')
+    else:
+        flash('Please log in to continue!') 
+        return redirect(url_for('index'))
+
 @app.route('/feed/', methods=["GET", "POST"])
 def viewPosts():
-    return render_template('feed.html',
-                           page_title='Posts')
+    #Only allow logged in users to view the feed
+    if 'user_id' in session:
+        conn = dbi.connect()
+        posts = homepage.getPostDetails(conn)
+        print(posts)
+    
+        if posts:
+            for info in posts:
+                userInfo = homepage.getUser(conn, info['user_id'])
+                info['name'] = userInfo['name']
+                # print(userInfo)
+                # print(info)
+
+            return render_template('feed.html',
+                            page_title='Posts',
+                            allPosts = posts)
+    #If they are not logged in, redirect to log in page with a message
+    else:
+        flash('You must be logged in to view the posts!')
+        return redirect(url_for('index'))
 
 """ @app.route('/profile/', methods=["GET", "POST"])
 def viewProfile():
